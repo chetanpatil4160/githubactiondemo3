0403bac082fac60c3b80d688c968da0ec2004f2d
chetan patil <chetanpatil4160@gmail.com>
2025-11-08 15:33:45 +0530

    testing1

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd 
 import os
 
-token=os.getenv("API_TOEKN")
+token=os.getenv("API_TOKEN") 
 
 #response = requests.get("https://jsonplaceholder.typicode.com/users")
 #data = response.json()
